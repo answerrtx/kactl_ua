8bda2107a938f3aa3e0c01b50462635ed9992617
answerrtx <weiying3@ualberta.ca>
2022-05-16 04:17:25 +0000

    modify pTree, add fwt

--- content/strings/PalindromicTree.h
+++ content/strings/PalindromicTree.h
@@ -1,59 +1,82 @@
 /**
- * Author: 9feng?
- * License: Unlicense
- * Source: https://zhuanlan.zhihu.com/p/43794156
- * Description: All palindromic substrings of the original string can be found within its structure
+ * Author: calabash_boy
+ * License: ————
+ * Source: https://github.com/4thcalabash/ACM-Code-Library/blob/master/String/Automaton/Palindromic_Automaton.cpp
+ * Description: [BZOJ 3676] All palindromic substrings of the original string can be found within its structure
  * Time: O(N)
  * Usage:
- *  ___
- * Status: ___
+ *  calc max(len(t)*cnt(t)) t is the palindromic subsring of s, and cnt(t)=t is the number of occurrences
+ * Status: tested
  */
 
- typedef long long ll;
-const int maxn=300010;
-char s[maxn];
-int n;
-struct Ptree {
-    int last;
-    struct Node {
-        int cnt, lenn, fail, son[27];
-        Node(int lenn, int fail):lenn(lenn), fail(fail), cnt(0){
-            memset(son, 0, sizeof(son));
-        };
-    };
-    vector<Node> st;
-    inline int newnode(int lenn, int fail=0) {
-        st.emplace_back(lenn, fail);
-        return st.size()-1;
+const int maxn = 3e5+100;
+struct Palindromic_AutoMaton{
+    //basic
+    int s[maxn],now;
+    int nxt[maxn][26],fail[maxn],l[maxn],last,tot;
+    // extension
+    int num[maxn];/*the number of occurrences*/
+    void clear(){
+        //1:odd length root, 0:even length root
+        s[0]=l[1]=-1;
+        fail[0] = tot = now =1;
+        last = l[0]=0;
+        memset(nxt[0],0,sizeof nxt[0]);
+        memset(nxt[1],0,sizeof nxt[1]);
     }
-    inline int getfail(int x, int n) {
-        while (s[n-st[x].lenn-1] != s[n]) x=st[x].fail;
+    Palindromic_AutoMaton(){clear();}
+    int newnode(int ll){
+        tot++;
+        memset(nxt[tot],0,sizeof nxt[tot]);
+        fail[tot]=num[tot]=0;
+        l[tot]=ll;
+        return tot;
+    }
+    int get_fail(int x){
+        while (s[now-l[x]-2]!=s[now-1])x = fail[x];
         return x;
     }
-    inline void extend(int c, int i) {
-        int cur=getfail(last, i);
-        if (!st[cur].son[c]) {
-            int nw=newnode(st[cur].lenn+2, st[getfail(st[cur].fail, i)].son[c]);
-            st[cur].son[c]=nw;
+    void add(int ch){
+        s[now++] = ch;
+        int cur = get_fail(last);
+        if(!nxt[cur][ch]){
+            int tt = newnode(l[cur]+2);
+            fail[tt] = nxt[get_fail(fail[cur])][ch];
+            nxt[cur][ch] = tt;
         }
-        st[ last=st[cur].son[c] ].cnt++;
+        last = nxt[cur][ch];num[last]++;
     }
-    void init() {
-        scanf("%s", s+1);
-        n=strlen(s+1);
-        s[0]=0;
-        newnode(0, 1), newnode(-1);
-        last=0;
-        rep(i, 1, n) extend(s[i]-'a', i);
+    void build(){
+        //fail[i]<i,Topology updates can be scanned monotonically.
+        for (int i=tot;i>=2;i--){
+            num[fail[i]]+=num[i];
+        }
+        num[0]=num[1]=0;
     }
-    ll count() {
-        drep(i, st.size()-1, 0) st[st[i].fail].cnt+=st[i].cnt;
-        ll ans=0;
-        rep(i, 2, st.size()-1) ans=max(ans, 1LL*st[i].lenn*st[i].cnt);
-        return ans;
+    void init(char* ss){
+        while (*ss){
+            add(*ss-'a');ss++;
+        }
     }
-}T;
-int main() {
-    T.init();
-    printf("%lld\n", T.count());
+    void init(string str){
+        for (int i=0;i<str.size();i++){
+            add(str[i]-'a');
+        }
+    }
+    long long query();
+}pam;
+long long Palindromic_AutoMaton::query(){
+    long long ret =1;
+    for (int i=2;i<=tot;i++){
+        ret = max(ret,1LL*l[i]*num[i]);
+    }
+    return ret;
+}
+char s[maxn];
+int main(){
+    scanf("%s",s);
+    pam.init(s);
+    pam.build();
+    printf("%lld\n",pam.query());
+    return 0;
 }
