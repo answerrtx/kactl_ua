0385893027cc0160fd476f95b74457f0de7631cc
IanDeHaan <idehaan@ualberta.ca>
2023-08-29 23:22:17 -0400

    more

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -28,6 +28,10 @@ vector<P> polygonCut(const vector<P>& poly, P s, P e) {
 	vector<P> res;
 	rep(i,0,sz(poly)) {
 		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
+		if (s.cross(e, cur) < EPS) {
+			res.push_back(cur);
+			continue;
+		}
 		bool side = s.cross(e, cur) < 0;
 		if (side != (s.cross(e, prev) < 0))
 			res.push_back(lineInter(s, e, cur, prev).second);
